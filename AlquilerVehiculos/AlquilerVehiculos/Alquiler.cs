96cb41c2aa22f39128ef085877ec57ebe6921e9c
Danimz777 <danielazulu03@outlook.com>
2024-09-16 15:35:36 -0500

    Menù del programa

--- AlquilerVehiculos/AlquilerVehiculos/Alquiler.cs
+++ AlquilerVehiculos/AlquilerVehiculos/Alquiler.cs
@@ -8,5 +8,36 @@ namespace AlquilerVehiculos
 {
     public class Alquiler
     {
+        private static List<Vehiculo> vehiculos = new List<Vehiculo>();
+        private static bool esAdministrador = false;
+
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Seleccione su rol:");
+                Console.WriteLine("1. Administrador");
+                Console.WriteLine("2. Cliente");
+                Console.Write("Ingrese una opción: ");
+                var rol = Console.ReadLine();
+
+                if (rol == "1")
+                {
+                    esAdministrador = true;
+                    Console.WriteLine("\nInicio de sesión como Administrador.\n");
+                    AdministradorMenu();
+                }
+                else if (rol == "2")
+                {
+                    esAdministrador = false;
+                    Console.WriteLine("\nInicio de sesión como Cliente.\n");
+                    ClienteMenu();
+                }
+                else
+                {
+                    Console.WriteLine("Opción no válida.");
+                }
+            }
+        }
     }
 }
