5453156d7f7b9c90626115bed55368ad496d993f
Danimz777 <danielazulu03@outlook.com>
2024-09-16 15:53:46 -0500

    Menu Admin

--- AlquilerVehiculos/AlquilerVehiculos/Alquiler.cs
+++ AlquilerVehiculos/AlquilerVehiculos/Alquiler.cs
@@ -39,5 +39,51 @@ namespace AlquilerVehiculos
                 }
             }
         }
+        private static void AdministradorMenu()
+        {
+            while (esAdministrador)
+            {
+                Console.WriteLine("--- Menú de Gestión de Vehículos ---");
+                Console.WriteLine("1. Registrar nuevo vehículo");
+                Console.WriteLine("2. Eliminar vehículo");
+                Console.WriteLine("3. Actualizar vehículo");
+                Console.WriteLine("4. Ver historial de reservas");
+                Console.WriteLine("5. Mostrar vehículos");
+                Console.WriteLine("6. Cambiar a Cliente");
+                Console.WriteLine("7. Salir");
+                Console.Write("Seleccione una opción: ");
+                var opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        RegistrarVehiculo();
+                        break;
+                    case "2":
+                        EliminarVehiculo();
+                        break;
+                    case "3":
+                        ActualizarVehiculo();
+                        break;
+                    case "4":
+                        VerHistorialReservas();
+                        break;
+                    case "5":
+                        MostrarVehiculos();
+                        break;
+                    case "6":
+                        esAdministrador = false;
+                        Console.WriteLine("\nCambiando a modo Cliente.\n");
+                        return; // Salir del menú de administrador
+                    case "7":
+                        Console.WriteLine("Saliendo del programa.");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida.");
+                        break;
+                }
+            }
+        }
     }
 }
